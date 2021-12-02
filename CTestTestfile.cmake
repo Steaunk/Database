c9b1cac4d88537ef9293969e5d70e2ba324a597b
王建楠 <wangjn19@mails.tsinghua.edu.cn>
2021-12-02 17:30:52 +0800

    rm CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/steaunk/THU/2021-Autumn/Introduction-to-Database-Management-System/Database
-# Build directory: /Users/steaunk/THU/2021-Autumn/Introduction-to-Database-Management-System/Database
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pf")
-subdirs("rm")
-subdirs("ix")
