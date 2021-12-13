6086a0dccf93f34b4cc5402e268e5da7fecbcc60
tshoigyr <573574239@qq.com>
2021-12-13 12:37:43 +0800

    make

---
+++ ix/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /media/tshoigyr/Work/thu/database/database/ix
+# Build directory: /media/tshoigyr/Work/thu/database/database/ix
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ix_test_1 "/media/tshoigyr/Work/thu/database/database/ix/ix_test_1")
+set_tests_properties(ix_test_1 PROPERTIES  _BACKTRACE_TRIPLES "/media/tshoigyr/Work/thu/database/database/ix/CMakeLists.txt;21;add_test;/media/tshoigyr/Work/thu/database/database/ix/CMakeLists.txt;0;")
